c529fd52657ba83b40fa4ce8febd4274a8ce68e1
Ben Muthalaly <benjamin.muthalaly@gmail.com>
2020-11-02 13:57:58 -0500

    Add second handle function and drop table at the end

--- minDB.go
+++ minDB.go
@@ -3,10 +3,14 @@ package main
 import (
 	"database/sql"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+func()
 
 func main() {
 	db, err := sql.Open("sqlite3", "/Users/moose1/Documents/photoApp/testDB")
@@ -24,6 +28,7 @@ func main() {
 	_, err = db.Exec("insert into foo (value) values (2)")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration(rand.Float64() * float64(time.Second)))
 		_, err := db.Exec("insert into foo (value) values (1)")
 		if err != nil {
 			log.Printf("failed to insert into db: %s", err)
@@ -32,8 +37,32 @@ func main() {
 		if err != nil {
 			log.Printf("failed to query db: %s", err)
 		}
+		func() {
+			r, err := http.NewRequest("GET", "localhost:8082/a")
+			if err != nil {
+				log.Printf("failed to reach /a: %s", err)
+			}
+
+		}()
 
 	})
 
+	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration(rand.Float64() * float64(time.Second)))
+		_, err := db.Exec("insert into foo (value) values (1)")
+		if err != nil {
+			log.Printf("failed to insert into db: %s", err)
+		}
+		_, err = db.Query("select * from foo")
+		if err != nil {
+			log.Printf("failed to query db: %s", err)
+		}
+		
+	})
+
 	http.ListenAndServe(":8082", nil)
+	_, err = db.Exec("drop table foo")
+	if err != nil {
+		log.Printf("failed to drop table: %s", err)
+	}
 }
